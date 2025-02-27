30971f46669a076cf9456213cf7f9c37f33ddb6b
allatester21 <allatester21@gmail.com>
2025-02-27 12:53:30 +0700

    Lesson-03-1

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -1,13 +1,20 @@
 from address import Address
 from mail import Mailing
 
-
-mail = Mailing("Moscow", "Kemerovo", 1500, "836543975")
 address = Address("650056", "Kemerovo", "Volgogradskaya", "150", "15")
 address_to = Address("490014", "Moscow", "Lenina", "23", "1")
 
-print(f"Отправление {mail.track} из {address.index}, {address.city}, "
-      f"{address.street}, - {address.flat} в "
-      f"{address_to.index}, {address_to.city}, {address_to.street}, "
-      f"{address_to.house} - {address_to.flat}."
+mail = Mailing(
+      to_address=address_to,
+      from_address=address,
+      cost=1500,
+      track="836543975"
+)
+
+print(f"Отправление {mail.track} из {mail.from_address.index}, "
+      f"{mail.from_address.city}, "
+      f"{mail.from_address.street}, - {mail.from_address.flat} в "
+      f"{mail.to_address.index}, {mail.to_address.city}, "
+      f"{mail.to_address.street}, "
+      f"{mail.to_address.house} - {mail.to_address.flat}."
       f"Стоимость {mail.cost} рублей")
